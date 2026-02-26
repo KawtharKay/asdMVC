d359ffcb41dd65b21eef59f6a31466b8d185ba6e
KawtharKay <kawtharkehinde@gmail.com>
2026-02-26 12:44:22 +0100

    Context

---
+++ Infrastructure/Persistence/Configurations/UserRoleTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class UserRoleTypeConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.HasOne(a => a.User)
+                .WithMany(a => a.UserRoles)
+                .HasForeignKey(a => a.UserId);
+
+            builder.HasOne(a => a.Role)
+                .WithMany(a => a.UserRoles)
+                .HasForeignKey(a => a.RoleId);
+        }
+    }
+}
