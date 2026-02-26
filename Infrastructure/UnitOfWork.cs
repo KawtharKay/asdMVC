b4f0ec145a06cd16653e5138727b7ac4c6e4f6ee
KawtharKay <kawtharkehinde@gmail.com>
2026-02-26 13:36:21 +0100

    okay

---
+++ Infrastructure/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public async Task<int> SaveAsync()
+        {
+            return await SaveAsync();
+        }
+    }
+}
